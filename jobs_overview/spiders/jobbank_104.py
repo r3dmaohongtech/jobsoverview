a4764f9f021e2062e3569e0d25141954749f037a
r3dmaohong <r3dmaohong@gmail.com>
2017-10-23 17:41:42 +0800

    maximun page

--- jobs_overview/spiders/jobbank_104.py
+++ jobs_overview/spiders/jobbank_104.py
@@ -9,7 +9,7 @@ class Spider104(BaseSpider):
     name = 'jobbank_104'
     
     #url_part1 = 'https://www.104.com.tw/jobs/search/list?ro=0&keyword=%E6%95%B8%E6%93%9A%E5%88%86%E6%9E%90%E5%B7%A5%E7%A8%8B%E5%B8%AB&area=6001001000%2C6001002000&order=1&asc=0&excludeJobKeyword=%E7%B3%BB%E7%B5%B1%E5%88%86%E6%9E%90%E5%B8%AB&kwop=7&page='
-    page_range = range(1,3)
+    page_range = range(1,6)
     #url_part2 = '&mode=s&jobsource=n104bank1'
     start_urls = ['https://www.104.com.tw/jobs/search/list?ro=0&keyword=%E6%95%B8%E6%93%9A%E5%88%86%E6%9E%90%E5%B7%A5%E7%A8%8B%E5%B8%AB&area=6001001000%2C6001002000&order=1&asc=0&excludeJobKeyword=%E7%B3%BB%E7%B5%B1%E5%88%86%E6%9E%90%E5%B8%AB&kwop=7&page='+str(i)+'&mode=s&jobsource=n104bank1' for i in page_range]
     
